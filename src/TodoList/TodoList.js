b047afc5f161b030ca6b813fc27c5693fa00ffd1
YutamaKotaro <california.dreamin.never.die@gmail.com>
2017-04-11 23:15:13 +0900

    :sparkles: create Blank Components

---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
